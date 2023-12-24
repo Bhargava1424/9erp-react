4e2855f115866faee058194fd01ec90e218e20ab
Bhargava1424 <sribhargava2002@gmail.com>
2023-12-24 15:09:33 +0530

    add student done 2

--- src/components/AddStudent.js
+++ src/components/AddStudent.js
@@ -20,7 +20,7 @@ const AddStudent = () => {
                         <span className="label-text" style={{ fontSize: '20px' }}>Student Name: </span>
                         <span className="label-text-alt"></span>
                     </div>
-                    <input type="text" placeholder="Student Name" className="input input-bordered w-full max-w-xs" />
+                    <input type="text" placeholder="Student Name" className="input input-bordered w-full max-w-xs" id='name'/>
                     <div className="label">
                         <span className="label-text-alt" style={{ fontSize: '15px' }}>⚠ name according to 10th certificate</span>
                         <span className="label-text-alt"></span>
@@ -34,7 +34,7 @@ const AddStudent = () => {
                         <span className="label-text" style={{ fontSize: '20px' }}>Father's Name: </span>
                         <span className="label-text-alt"></span>
                     </div>
-                    <input type="text" placeholder="Father's Name:" className="input input-bordered w-full max-w-xs" />
+                    <input type="text" placeholder="Father's Name:" className="input input-bordered w-full max-w-xs" id='parentName'/>
                     <div className="label">
                         <span className="label-text-alt" style={{ fontSize: '15px' }}>⚠ father's name according to 10th certificate</span>
                         <span className="label-text-alt"></span>
@@ -48,7 +48,7 @@ const AddStudent = () => {
                         <span className="label-text" style={{ fontSize: '20px' }}>Primary Contact: </span>
                         <span className="label-text-alt"></span>
                     </div>
-                    <input type="text" placeholder="Primary Contact" className="input input-bordered w-full max-w-xs" />
+                    <input type="text" placeholder="Primary Contact" className="input input-bordered w-full max-w-xs" id='primaryNumber'/>
                     <div className="label">
                         <span className="label-text-alt" style={{ fontSize: '15px' }}>⚠ number must have 10 digits</span>
                         <span className="label-text-alt"></span>
@@ -62,7 +62,7 @@ const AddStudent = () => {
                         <span className="label-text" style={{ fontSize: '20px' }}>Secondary Contact: </span>
                         <span className="label-text-alt"></span>
                     </div>
-                    <input type="text" placeholder="Secondary Contact" className="input input-bordered w-full max-w-xs" />
+                    <input type="text" placeholder="Secondary Contact" className="input input-bordered w-full max-w-xs" id='secondaryNumber'/>
                     <div className="label">
                         <span className="label-text-alt" style={{ fontSize: '15px' }}>⚠ number must have 10 digits</span>
                         <span className="label-text-alt"></span>
@@ -76,13 +76,13 @@ const AddStudent = () => {
                     <label class="label cursor-pointer">
                         
                         <span class="label-text" style={{ fontSize: '20px' }}>Male:----------</span>
-                        <input type="radio" name="radio-10" class="radio checked:bg-blue-500" checked />
+                        <input type="radio" id='male' name="radio-10" class="radio checked:bg-blue-500" checked />
                     </label>
                     </div>
                     <div class="form-control">
                     <label class="label cursor-pointer">
                         <span class="label-text" style={{ fontSize: '20px' }}>Female-------</span> 
-                        <input type="radio" name="radio-10" class="radio checked:bg-pink-500" checked />
+                        <input type="radio" id='female' name="radio-10" class="radio checked:bg-pink-500" checked />
                     </label>
                 </div>
             </div> 
@@ -90,7 +90,7 @@ const AddStudent = () => {
             <div class="grid h-30 card bg-base-300 rounded-box place-items-center">
             <p class="text-lg mb-4 mt-5" >Year Of Joining</p>
                 <div class="m-4">
-                    <select class="select select-info w-full max-w-xs">
+                    <select class="select select-info w-full max-w-xs" id='yearOfJoining'>
                         {Array.from({ length: 80 }, (_, index) => (
                             <option key={index + 2021}>{2021 + index}</option>
                         ))}
@@ -101,7 +101,7 @@ const AddStudent = () => {
             <div class="grid h-30 card bg-base-300 rounded-box place-items-center">
                 <div class="m-4">
                     <p class="text-lg mb-4">Batch</p>
-                    <select class="select select-info w-full max-w-xs">
+                    <select class="select select-info w-full max-w-xs" id='batch'>
                         <option disabled selected>Select Batch</option>
                         <option>Upcoming</option>
                         <option>Junior</option>
@@ -115,7 +115,7 @@ const AddStudent = () => {
             <div class="grid h-30 card bg-base-300 rounded-box place-items-center">
                 <div class="m-4">
                     <p class="text-lg mb-4">Course</p>
-                    <select class="select select-info w-full max-w-xs">
+                    <select class="select select-info w-full max-w-xs" id="course">
                         <option disabled selected>Select Course</option>
                         <option>MPC</option>
                         <option>BiPC</option>
